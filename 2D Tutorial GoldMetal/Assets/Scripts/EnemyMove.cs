ffb3ec5a9257a3b8c97f335afe119b887fab09b5
lew0205 <lew0205@naver.com>
2021-09-09 20:13:47 +0900

    .

--- 2D Tutorial GoldMetal/Assets/Scripts/EnemyMove.cs
+++ 2D Tutorial GoldMetal/Assets/Scripts/EnemyMove.cs
@@ -7,48 +7,70 @@ public class EnemyMove : MonoBehaviour
     Rigidbody2D rigid;
     Animator anim;
     SpriteRenderer spriteRenderer;
+    CapsuleCollider2D capcollider;
     public int nextMove;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        anim=GetComponent<Animator>();
-        spriteRenderer=GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        capcollider = GetComponent<CapsuleCollider2D>();
 
         Think();
     }
 
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(nextMove,rigid.velocity.y);
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
 
-        Vector2 frontVec = new Vector2(rigid.position.x+nextMove*0.3f, rigid.position.y);
-        RaycastHit2D rayHit = Physics2D.Raycast(frontVec,Vector3.down,1,LayerMask.GetMask("Platform"));
-        if(rayHit.collider == null){
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.3f, rigid.position.y);
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        if (rayHit.collider == null)
+        {
             Turn();
         }
     }
 
     void Think()
     {
-        nextMove = Random.Range(-1,2);
+        nextMove = Random.Range(-1, 2);
 
-        anim.SetInteger("WalkSpeed",nextMove);
-        if(nextMove!=0)
+        anim.SetInteger("WalkSpeed", nextMove);
+        if (nextMove != 0)
             spriteRenderer.flipX = nextMove == 1;
 
-        float nextThinkTime = Random.Range(2f,5f);
-        Invoke("Think",nextThinkTime);
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
     }
 
     void Turn()
     {
-            nextMove*=-1;
-            spriteRenderer.flipX = nextMove ==1;
+        nextMove *= -1;
+        spriteRenderer.flipX = nextMove == 1;
 
-            CancelInvoke();
-            float nextThinkTime = Random.Range(2f,5f);
-            Invoke("Think",nextThinkTime);
+        CancelInvoke();
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
 
+    }
+
+    public void OnDamaged()
+    {
+        // Sprite Alpha
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+        // Sprite Flip Y
+        spriteRenderer.flipY = true;
+        // Collider Disable
+        capcollider.enabled = false;
+        // Die Effect Jump
+        rigid.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+        // Destroy
+        Invoke("DeActive", 5);
+    }
+
+    void DeActive()
+    {
+        gameObject.SetActive(false);
     }
 }
